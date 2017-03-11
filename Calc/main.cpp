e4cf788fc7aa73e8d74ffc5a39dafd3c3f3c70d6
Silence <youcankeepsilence@gmail.com>
2017-03-11 10:31:52 +0300

    Допилил очередь (вроде как)

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -9,13 +9,24 @@ int main(int argc, char *argv[])
 {
     cout << "Hello World!" << endl;
     Lecsema * p;
+    NQueue * queue;
     Number * n;
+    Number * n1;
+    n1=new Number;
+    n1->setNum(3.32);
     n=new Number;
     n->setNum(2.28);
     p=dynamic_cast<Lecsema*>(n);
-    //Суй в очередь
+    queue=new NQueue(p);
+    p=dynamic_cast<Lecsema*>(n1);
+    queue->AddToQueue(p);
+    p=nullptr;
+    n=nullptr;
+    p=queue->GetFromQueue();
     n=dynamic_cast<Number*>(p);
-
-    cout<<"Hi"<<n->getNum();
+    cout<<"Hi"<<n->getNum()<<endl;
+    p=queue->GetFromQueue();
+    n=dynamic_cast<Number*>(p);
+    cout<<n->getNum();
     return 0;
 }
--- Calc/nqueue.h
+++ Calc/nqueue.h
@@ -1,18 +1,19 @@
 #ifndef NQUEUE_H
 #define NQUEUE_H
-
+#include "lecsema.h"
 
 
 class NQueue
 {
 public:
-    NQueue();
-//    NQueue();
+    NQueue(Lecsema *data=nullptr);
+    void AddToQueue(Lecsema *data);
+    Lecsema * GetFromQueue(bool &isOver);
+private:
+    NQueue * p_current;
     NQueue * p_next;
     NQueue * p_first;
-//    void AddToQueue(T data);
-private:
-//    T m_data;
+    Lecsema *m_data;
 };
 
 #endif // NQUEUE_H
